69789fa597629e1dee223bf95f2ea520610c55d1
Gabriel Olmos Gil <gabrielolmgil@campus.monlau.com>
2025-01-31 19:25:10 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { FeaturedComponent } from './components/featured/featured.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,NavBarComponent,FeaturedComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
