a63c9a4a271005f0c0ebfc63eb2143ff2fd3904e
Gabriel Olmos Gil <gabrielolmgil@campus.monlau.com>
2025-02-14 18:10:36 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { FeaturedComponent } from './components/featured/featured.component';
+import { Body1Component } from './components/body1/body1.component';
+import { Component2Component } from './components/component2/component2.component';
+import { Component3Component } from './components/component3/component3.component';
+import { Component4Component } from './components/component4/component4.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,NavBarComponent,FeaturedComponent],
+  imports: [NavBarComponent,FeaturedComponent,Body1Component,Component2Component,Component3Component, Component4Component, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/components/body1/body1.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-body1',
+  imports: [],
+  templateUrl: './body1.component.html',
+  styleUrl: './body1.component.css'
+})
+export class Body1Component {
+
+}
